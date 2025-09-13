413f13fe92f7aba5dd8c2ffb948e0f90fd12bb58
JamesYu17 <jamesc.yu04@gmail.com>
2025-09-14 02:46:36 +0800

    Delete YU/assets directory

--- YU/assets/script.js
+++
@@ -1,9 +0,0 @@
-function validateForm() {
-  let Surname = document.forms["regform"]["Surname"].value;
-  let Firstname = document.forms["regform"]["Firstname"].value;
-  let mail = document.forms["regform"]["mail"].value;
-  if (Surname == "" || Firstname == "" || mail == "") {
-    alert("Name(s) must be filled out");
-    return false;
-  }
-}
